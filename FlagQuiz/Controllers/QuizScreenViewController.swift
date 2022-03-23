f8e8bbffc50a6cdd3f8c4919d18535efdfeb93ef
Cihan ÇALLI <cihan.callii@gmail.com>
2022-03-23 16:38:13 +0300

    First Commit

---
+++ FlagQuiz/Controllers/QuizScreenViewController.swift
@@ -0,0 +1,31 @@
+//
+//  QuizScreenViewController.swift
+//  FlagQuiz
+//
+//  Created by Cihan Çallı on 23.03.2022.
+//
+
+import UIKit
+
+class QuizScreenViewController: UIViewController {
+    
+    @IBOutlet weak var correctTextLabel: UILabel!
+    @IBOutlet weak var wrongTextLabel: UILabel!
+    @IBOutlet weak var questionTextLabel: UILabel!
+    @IBOutlet weak var flagImageView: UIImageView!
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    @IBAction func didAButtonTapped(_ sender: Any) {
+    }
+    @IBAction func didBButtonTapped(_ sender: Any) {
+    }
+    @IBAction func didCButtonTapped(_ sender: Any) {
+    }
+    @IBAction func didDButtonTapped(_ sender: Any) {
+    }
+    
+}
---
+++ FlagQuiz/Controllers/ResultScreenViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ResultScreenViewController.swift
+//  FlagQuiz
+//
+//  Created by Cihan Çallı on 23.03.2022.
+//
+
+import UIKit
+
+class ResultScreenViewController: UIViewController {
+    
+    @IBOutlet weak var resultTextLabel: UILabel!
+    @IBOutlet weak var successTextLabel: UILabel!
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    @IBAction func didTrayAgainButtonTapped(_ sender: Any) {
+    }
+}
